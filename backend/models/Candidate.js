14b757efe8c451b376dc0801051945b769c7d770
irvinjs22620075 <22620075@tlaxiaco.tecnm.mx>
2025-11-30 23:34:17 -0600

    Añadiendo lista para seleccionar candidatos y mejorando la seccion de votacion

--- backend/models/Candidate.js
+++ backend/models/Candidate.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const CandidateSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true },
+    rfc: { type: String }, // Almacena el email del usuario
+    wallet_address: { type: String },
     party: { type: String },
     description: { type: String },
     imageUrl: { type: String },
