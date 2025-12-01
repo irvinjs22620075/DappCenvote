463eacfebab9b7bcb50b94bd5b4d2d4f617efa9f
irvinjs22620075 <22620075@tlaxiaco.tecnm.mx>
2025-11-30 22:37:51 -0600

    Subiendo cambios, funciona registro de usuario, registro de candidatos y creacion de encuestas, se guaradan en la bd

--- backend/models/Survey.js
+++ backend/models/Survey.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 
 const SurveySchema = new mongoose.Schema({
     _id: { type: String, required: true },
-    title: { type: String, required: true },
+    name: { type: String, required: true },
     description: { type: String },
     candidates: [{ type: String, ref: 'Candidate' }],
-    startDate: { type: Date },
-    endDate: { type: Date },
+    start_date: { type: Date },
+    end_date: { type: Date },
     isActive: { type: Boolean, default: true },
+    vote_fee: { type: Number, default: 0.1 },
+    created_by: { type: String },
     created_at: { type: Date, default: Date.now }
 });
 
